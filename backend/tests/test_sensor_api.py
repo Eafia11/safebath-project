5bf375bb4a6a5c664a6f8e6d349047aeb65cb23a
mille <miller101953@gmail.com>
2026-06-02 17:17:04 +0900

    Use 20 second fall response timeout

--- backend/tests/test_sensor_api.py
+++ backend/tests/test_sensor_api.py
@@ -143,7 +143,7 @@ def test_possible_fall_becomes_emergency_when_button_is_not_pressed(client):
         },
     )
 
-    state_service.abnormal_start_time = datetime.utcnow() - timedelta(seconds=11)
+    state_service.abnormal_start_time = datetime.utcnow() - timedelta(seconds=21)
     timeout_response = client.post(
         "/sensor/mmwave",
         json={
